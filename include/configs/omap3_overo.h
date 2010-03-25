422c06888087770ebb0cfde37f1fbb846aa0640e
Steve Sakoman <steve@sakoman.com>
2010-03-25 06:54:47 -0700

    OMAP3: Beagle, Overo: remove omapfb.debug=y from default env

--- include/configs/omap3_overo.h
+++ include/configs/omap3_overo.h
@@ -164,7 +164,6 @@
 		"mpurate=${mpurate} " \
 		"vram=${vram} " \
 		"omapfb.mode=dvi:${dvimode} " \
-		"omapfb.debug=y " \
 		"omapdss.def_disp=${defaultdisplay} " \
 		"root=${mmcroot} " \
 		"rootfstype=${mmcrootfstype}\0" \
@@ -172,7 +171,6 @@
 		"mpurate=${mpurate} " \
 		"vram=${vram} " \
 		"omapfb.mode=dvi:${dvimode} " \
-		"omapfb.debug=y " \
 		"omapdss.def_disp=${defaultdisplay} " \
 		"root=${nandroot} " \
 		"rootfstype=${nandrootfstype}\0" \
